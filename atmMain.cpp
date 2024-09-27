11a9d5bffeb365a586c7e7d43432ac16e5ae7422
Kyohi <160745497+Kyohi505@users.noreply.github.com>
2024-09-27 21:17:49 +0800

    Create atmMain.cpp

---
+++ atmMain.cpp
@@ -0,0 +1,240 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <limits>
+#include <ctime>
+#include <set>
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::getline;
+
+struct Acc{
+    string name;
+    string bday;
+    string contact;
+    string accNum;
+    string pinCode;
+    double balance;
+};
+
+struct Node{
+    Acc data;
+    Node* next;
+    Node(Acc x) : data(x), next(NULL){}
+};
+
+class System{
+    private:
+        Node* head;
+        //Node* currentUser;
+        int createAccNumber();
+        std::set<int> generatedAccNumbers;
+    public:
+        System() : head(NULL){}//currentUser(NULL){}
+        void registerAcc();
+        void enterAcc(string n, string p);
+        void checkRegister();
+        void showBalance();
+        int withdraw();
+        void deposit(double x);
+        void fundTransfer(int x);
+        void changePin();
+        void showAcc(string x);
+        void machineMenu();
+
+};
+
+void System::registerAcc(){
+
+Acc x;
+cout<<"Input name: ";
+cin.ignore();
+getline(cin, x.name);
+
+cout <<"Input Birth Date(MM/DD/YY): "; cin >> x.bday;
+cout <<"Input Contact Number: "; cin >> x.contact;
+cout <<"Input Pin: "; cin >> x.pinCode;
+x.accNum = std::to_string(createAccNumber());
+
+Node* p, *q, *newNode;
+p = q = head;
+
+newNode = new Node(x);
+
+while(p != NULL){
+    q = p;
+    p = p->next;
+}
+
+if(p == head)
+    head = newNode;
+else{
+    q->next = newNode;
+    newNode->next = p;
+    }
+
+showAcc(x.accNum);
+
+}
+
+int System::createAccNumber(){
+    int accNum;
+
+    do{
+        accNum = rand() % 90000 + 10000;
+    }while(generatedAccNumbers.count(accNum));
+
+    generatedAccNumbers.insert(accNum);
+    return accNum;
+}
+
+void System::enterAcc(string n, string pin){
+Node* p, *q;
+p = q = head;
+while(p != NULL && n != p->data.accNum && pin !=p->data.pinCode){
+        q = p;
+        p = p->next;
+    }
+
+if(p == NULL){
+    cout << "Unsuccessful. PLS TRY AGAIN";
+    return;
+    }
+else{
+    cout<<"Account Login Successful";
+    //p = currentUser;
+    showAcc(n);
+}
+
+}
+
+
+void System::showBalance(){
+
+}
+
+void System::showAcc(string x){
+Node *p, *q;
+p = q = head;
+
+while (p != NULL && p->data.accNum != x){
+    q = p;
+    p = p->next;
+  }
+if (p==NULL){
+    cout<<"Acc not found.\n";
+    system("pause");
+}
+
+else{
+    system("cls");
+    cout << "Your acc number is: " << p->data.accNum << '\n';
+    cout << "Acc name: " << p->data.name << '\n';
+    cout << "Bday: " << p->data.bday << '\n';
+    cout << "Contact: " << p->data.contact << '\n';
+    cout << "Pin code: " << p->data.pinCode << '\n';
+
+    system("pause");
+}
+
+}
+
+int System::withdraw(){
+    return 0;
+}
+
+void System::deposit(double x){
+
+}
+
+void System::fundTransfer(int x){
+
+}
+
+void System::changePin(){
+
+}
+
+void System::machineMenu(){
+
+    while(true){
+
+        switch(mainMenu()){
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            default:
+        }
+    }
+}
+
+int registerMenu(){
+    int op;
+
+    system("cls");
+
+    cout<<"Menu\n";
+    cout<<"1. Not Register Acc\n";
+    cout<<"2. Open Acc\n";
+    cout<<"-> ";
+
+    cin >> op;
+    return op;
+}
+int mainMenu(){
+    int op;
+
+    system("cls");
+
+    cout<<"Menu\n";
+    cout<<"1. ---\n";
+    cout<<"2. ---\n";
+    cout<<"3. ---\n"; 
+    cout<<"4. ---\n";
+    cout<<"5. ---\n";
+    cout<<"6. ---\n"; 
+    cout<<"7. Exit\n";
+    cout<<"Select(1-7)\n ";
+    cout<<"-> ";
+
+    cin >> op;
+    return op;
+}
+
+int main(){
+
+System atm;
+string num, pin;
+    while(true){
+
+        switch(registerMenu()){
+        case 1:
+            system("cls");
+            atm.registerAcc();
+            cout << "Successful Account Registration\n";
+            system("pause");
+            break;
+
+        case 2:
+            system("cls");
+            cout<<"Enter Account Number: "; cin >> num;
+            cout<<"Enter Pin: "; cin >> pin;
+            atm.enterAcc(num, pin);
+            break;
+
+        default:
+            cout<<"Invalid input.\n";
+            cin.clear();
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            system("pause");
+        }
+    }
+
+    return 0;
+}
